bafde4d3744d8c10a8352f468fbd7bd2bc442736
Ricardo Alves <ras@ic.ufal.br>
2021-10-09 19:26:04 -0300

    chore: initial config

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    background: '#f2f2f2',
+    text: '#020202',
+    primary: '#ed2f59',
+    secondary: '#32335b',
+    success: '#4caf50',
+    error: '#ff5252',
+    warning: '#ffc107'
+  }
+}
+
+export default theme

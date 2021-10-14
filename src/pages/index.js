3c939ed12c328e40eba2a090fa25487a9937003f
Ricardo Alves <ras@ic.ufal.br>
2021-10-14 01:39:49 -0300

    feat(homepage): adds actions Visit weather or saved music pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
-export default function Home () {
+import { Header, Box } from 'styles/pages/homeStyle'
+import { Container } from 'styles/base'
+
+import WeatherInfoIcon from '../assets/weather.svg'
+import FavoriteIcon from '../assets/playlist.svg'
+
+export default function Home() {
   return (
     <div>
       <Head>
@@ -9,6 +16,33 @@ export default function Home () {
       </Head>
 
       <main>
+        <Container>
+          <Header>
+            <span className="subtitle">Bem vindo ao</span>
+            <h1 className="title">Mood</h1>
+          </Header>
+
+          <Box>
+            <Link href="/weather">
+              <div>
+                <p>
+                  Veja o <strong>clima</strong> na sua
+                  <br /> cidade e qual a melhor <strong>música</strong>
+                  <br /> para o momento
+                </p>
+                <WeatherInfoIcon />
+              </div>
+            </Link>
+            <Link href="/saved">
+              <div>
+                <p>
+                  Veja suas <strong>playlists</strong> salvas
+                </p>
+                <FavoriteIcon className="playlist-icon" />
+              </div>
+            </Link>
+          </Box>
+        </Container>
       </main>
     </div>
   )

7bd0e374757d5260692a7d1d2c209e369e4bb0b8
Ricardo Alves <ras@ic.ufal.br>
2021-10-14 01:23:58 -0300

    chore: update config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
-module.exports = {
-  reactStrictMode: true,
-}
-
 const withImages = require('next-images')
 
 module.exports = withImages({
   esModule: true
 })
+
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    RAPID_API_HOST: process.env.RAPID_API_HOST,
+    RAPID_API_KEY: process.env.RAPID_API_KEY,
+    OPEN_WEATHER_KEY: process.env.OPEN_WEATHER_KEY
+  }
+}
